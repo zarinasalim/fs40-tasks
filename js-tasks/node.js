f99dbd68c24b8d7ba190c58e2d8e276e94cccf7b
zarinasalim <salimovazarina72@gmail.com>
2025-05-24 02:47:01 +0400

    add new task

---
+++ js-tasks/node.js
@@ -0,0 +1,83 @@
+//console.log(My name is)
+
+//const name="Firuz";
+//console.log(name)
+
+//let bookAmount = 75;
+//let bookPrice = 25 ;
+//let totalPrice= bookAmount*bookPrice;
+//console.log(totalPrice) 
+
+//let kmhSpeed = 60;
+//let msSpeed = kmhSpeed * 1000 / 3600;
+//console.log(msSpeed)
+
+
+//let bdYear = +prompt ("Dogum tarixinizi qeyd edin!")
+//let age = 2025 - bdYear;
+//let days = age * 365;
+//console.log(age)
+
+
+//let firstName = "Zarina";
+//let lastName = "Salimova";
+//console.log(`Sizin adiniz ${firstName} ${lastName} Tanışlığımıza çox şadam!`)
+
+
+//let firstName = "Zarina ";
+//let lastName = "Salimova";
+//let message = (`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}`)
+//console.log(message)
+
+
+//let num = 12;
+//let power = num ** 5;
+//let remainder = num % 3;
+//console.log(` 5ci derece quvveti ${power}`)
+//console.log (` qalig ${remainder}`)
+
+
+//let fullName = "Zarina Salimova";
+//let herfsayi = fullName.replace(/\s/g, "").length;
+//console.log(`Adınızdakı hərflərin sayı:, ${herfsayi}`);
+
+
+//let fullName = "Reshad Farhadli"
+//let boshluq = fullName.split(" ");
+//let firstName = boshluq [0][0];
+//let lastName = boshluq [1][0];
+//console.log( firstName+lastName);
+
+
+//let fullName = +prompt ("Adinizi AD SOYAD olaraq daxil edin.")
+//let par = fullName.split(" ");
+//if (par.length === 2) {
+ // let ad = par[0];
+ // let soyad = par[1];
+ //console.log(soyad + " " + ad); 
+//} else {
+ // console.log("Zəhmət olmasa yalnız AD və SOYAD daxil edin.");
+//}
+
+
+//let fullName = prompt("Adini daxil ele!")
+//console.log(`Salam, ${fullName}`);
+
+
+//let eded1 = prompt("Birinci ədədi daxil edin:");
+//let eded2 = prompt("İkinci ədədi daxil edin:");
+//let a = Number(eded1);
+//let b = Number(eded2);
+//console.log("Toplama: " + (a + b));
+//console.log("Çıxma: " + (a - b));
+//console.log("Vurma: " + (a * b));
+//console.log("Bölmə: " + (a / b));
+
+
+let hasil = 12345 * 98765;
+let hasilReq = hasil.toString();
+let reqem = hasilReq[5];
+console.log("Hasil:" + hasil);
+console.log("6 ci reqem "  + reqem)
+ 
+
