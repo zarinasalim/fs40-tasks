10f9b425e4db5e7e64a610525a81e28b2dbb07c0
zarinasalim <salimovazarina72@gmail.com>
2025-05-25 21:36:08 +0400

    changed task

--- js-tasks/node.js
+++ js-tasks/node.js
@@ -74,10 +74,96 @@
 //console.log("Bölmə: " + (a / b));
 
 
-let hasil = 12345 * 98765;
-let hasilReq = hasil.toString();
-let reqem = hasilReq[5];
-console.log("Hasil:" + hasil);
-console.log("6 ci reqem "  + reqem)
+//let hasil = 12345 * 98765;
+//let hasilReq = hasil.toString();
+//let reqem = hasilReq[5];
+//console.log("Hasil:" + hasil);
+//console.log("6 ci reqem "  + reqem)
  
+// "" + 1 + 0 = 10;
+// "" - 1 + 0 = -1;
+// "true"-false = NaN;
+// 6 / "3" = 2;
+// 4 + 5 + "px" = "9px";
+// "$" + 4 + 5 = "$45";
+// "4" - 2 = 2;
+// NaN + "px" = "Nanpx";
+// 7 / 0 = Infinity;
+// "-9" + 5 = "-95";
+// "-9" - 5 = -14;
+// null + 1 = 1;
+// undefined + 1 = NaN;
 
+// let prom = prompt("Dord reqemli eded daxil edin!");
+// let sum = 0;
+// sum += Number(prom[0]);
+// sum += Number(prom[1]);
+// sum += Number(prom[2]);
+// sum += Number(prom[3]);
+// console.log("Cemi:  " + sum)
+
+// let firstName = prompt("Soyadinizi qeyd edin!");
+// let lastName = prompt("Adinizi qeyd edin");
+// console.log("Salam " + firstName + "!");
+
+
+// let price = prompt('Qiyməti "530.90" formatında daxil edin:');
+// let parts = price.split('.');
+// let manat = parts[0];
+// let qepik = parts[1] || "00";
+// if (qepik.length === 1) {
+//   qepik += "0";
+// }
+// console.log(manat + " manat " + qepik + " qəpik");
+
+
+// let vergi = prompt("maasi qeyd ele !");
+// let vergifaizli = vergi - (vergi * 0.13);
+// console.log ("Vergisiz emek haqqiniz " + vergifaizli);
+
+
+// let lastName = prompt ("Soyadinizi qeyd edin");
+// let firstName = prompt (" Adinizi qeyd edin");
+// let ataName= prompt(" Ata adini qeyd edin");
+// console.log ( lastName + firstName[0]  + lastName [0]);
+
+// let mebleg = prompt("Əmanətin məbləğini daxil edin:");
+// let muddet = prompt("Müddəti neçə ay olduğunu daxil edin:");
+// let faiz = prompt("Faiz dərəcəsini daxil edin");
+// let illikFaiz = faiz / 100;
+// let ayliqFaiz = illikFaiz / 12;
+// let menfeet = mebleg * ayliqFaiz * muddet;
+// console.log("Gözlənilən mənfəət: " + menfeet + " AZN");
+
+
+
+// let value = 10;
+// console.log(value < 15);            
+// console.log(value <= 10);           
+// console.log(value === 10);          
+// console.log(value != 20);          
+// console.log(value >= 5 && value <= 10); 
+// value = 20;
+// console.log(value < 15);            
+// console.log(value <= 10);           
+// console.log(value === 10);          
+// console.log(value != 20);           
+// console.log(value >= 5 && value <= 10);
+
+
+
+// let age = prompt("Yasinizi qeyd edin");
+// if(age > 18) {
+//   alert( "access denied");
+// }
+// else { 
+//  alert(" access grated");
+// }
+
+
+// let il = +prompt("İl daxil edin:");
+// if (il % 4 === 0) {
+//   console.log("İl uzun ildir.");
+// } else {
+//   console.log("İl uzun il deyil.");
+// }
