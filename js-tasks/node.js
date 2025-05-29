1ffdf2fc5ccd1dbb2b84f200477603cd8e108785
zarinasalim <salimovazarina72@gmail.com>
2025-05-29 17:15:13 +0400

    Added new tasks

--- js-tasks/node.js
+++ js-tasks/node.js
@@ -167,3 +167,215 @@
 // } else {
 //   console.log("İl uzun il deyil.");
 // }
+
+
+// let(id) = +prompt("enter product id;");
+// switch(id){
+//     case 1:
+//      alert("Available 10 pcs.");
+//      break;
+//      case 2:
+//     alert("Available 256 pcs.");
+//     break;
+//     case 3:
+//     alert("Available 256 pcs.");
+//     case 4:
+//     alert("There are 3 available.");
+//     break;
+//     default:
+//     alert("Out of stock"); 
+// }
+
+// let gender= prompt("Cinsiniz nedir?","F");
+// gender === "M"? console.log ( "Male" ) : gender === "F"? console.log ("Female") :console.log("Yanlish deyer");
+
+
+// let months= +prompt("Ayin nomresini qeyd edin:");
+// switch (months) {
+//     case 1:
+//        console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+// }
+
+// task-30
+// let date = prompt("Tarixi 2009.12.19 formasinda daxil edin");
+// let parts = date.split(".");
+// let year = parts[0];
+// let month = parts[1];
+// let day = parts[2];
+// if (month <= 12 && month >= 1 && day >= 1 && day <= 30){
+//     switch(month){
+//             case "1":
+//        console.log("January");
+//         break;
+//     case "2":
+//         console.log("February");
+//         break;
+//     case "3":
+//         console.log("March");
+//         break;
+//     case "4":
+//         console.log("April");
+//         break;
+//     case "5":
+//         console.log("May");
+//         break;
+//     case "6":
+//         console.log("June");
+//         break;
+//     case "7":
+//         console.log("July");
+//         break;
+//     case "8":
+//         console.log("August");
+//         break;
+//     case "9":
+//         console.log("September");
+//         break;
+//     case "10":
+//         console.log("October");
+//         break;
+//     case "11":
+//         console.log("November");
+//         break;
+//     case "12":
+//         console.log("December");
+//         break;
+//         default:
+//             console.log("format yanlisdir!");
+            
+//     }
+// }
+
+// task31
+// let p = 8;
+// let q = p-- - --p + p++ + ++p;
+// console.log(p, q); 8-16;
+
+// task32
+// // let a = 6, b = 4, c = 2;
+// // let x = ++a - b-- + c++ - --b + a++;
+// // let y = a-- + --c - ++b + x++ - --a;
+// // console.log(a, b, c, x, y); 
+// x = 7-4+2-2+7 = 10;
+// y = 8+2-3+10-6=11
+
+
+// task33
+// let a = 7, b = 3, c = 5;
+
+// let x = a-- - ++b + c++ - --a + b++ - --c + a++ - b-- + c++;
+// let y = --x + a++ - --b + c-- - x++ + ++a - b++ + --c - x--;
+// let z = a-- + ++b - --c + x++ - --y + b-- - ++x + c++ - --a + y++;
+
+// console.log(a, b, c, x, y, z);
+
+// x = 7-4+5-5+4-5+5-5+5=7
+// x = 7; a = 6; b = 4; c = 6;
+// y = 6+6-3+6-6+8-3+4-7= 11
+// a = 8; b = 4; c = 4; x = 6; y = 11;
+// z = 8+5-3+6-10+5-8+3-6+10=10
+// b = 4; x = 8; c = 4; a = 6; y = 11
+
+
+// task34
+// let eded = Number(prompt("Ədədi daxil edin:"));
+// if (eded > 0) {
+//   console.log("Ədəd müsbətdir.");
+// } else if (eded < 0) {
+//   console.log("Ədəd mənfidir.");
+// } else {
+//   console.log("Ədəd sıfırdır.");
+// }
+
+
+// task35
+// let a = 5, b = 8, c = 3;
+// let boyuk;
+// if (a >= b && a >= c) {
+//   boyuk = a;
+// } else if (b >= a && b >= c) {
+//   boyuk = b;
+// } else {
+//   boyuk = c;
+// }
+// console.log("Ən böyük ədəd:", boyuk);
+
+
+// task36
+// let eded = prompt("Eded daxil edin:");
+// let result = (eded % 2 === 0)? alert("Eded Cutdur") : alert(" Eded Tekdir");
+// console.log(result);
+
+
+// // task38
+// let qiymet = +prompt("Qiymetinizi qeyd edin:");
+// switch(true){
+//     case (qiymet >=90 && qiymet <=100):
+//     console.log("Ela");
+//     break;
+//     case (qiymet >=75 && qiymet <=89):
+//      console.log("Yaxsi");
+//      break;
+//      case (qiymet >=60 && qiymet <= 74):
+//     console.log("Orta");
+//      break;
+//     case (qiymet >=0 && qiymet <= 59):
+//      console.log("Zeif");
+//      break;
+//      default:
+//         console.log("Yanlishdir.");
+// }
+
+
+// task40
+// let a=5, b=3;
+// let operation="+";
+// let result;
+// switch(operation){
+//    case "+":
+//     result = a + b;
+//     break;
+//   case "-":
+//     result = a - b;
+//     break;
+//   case "*":
+//    result = a * b;
+//     break;
+//     default:
+//     result = "Yanlshdir.";
+// }
+// alert("Operator:" + operation + "   " + "Netice:" + result);
