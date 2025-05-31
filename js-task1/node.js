94a0fb2f4297c829d41fe8e95d751584b5f12db4
zarinasalim <salimovazarina72@gmail.com>
2025-05-31 18:03:53 +0400

    js-task-1

---
+++ js-task1/node.js
@@ -0,0 +1,52 @@
+// task1
+// let eded = prompt("ikireqemli eded daxil edin");
+// while (eded < 100){
+//  eded += 7;
+// }
+// console.log(eded)
+
+
+
+// task2
+// let number = prompt("nece defe cixardilsin?");
+// let i = 0;
+// while (i < number) {
+//     console.log ("I know how to use cycles");
+//     i++;
+// }
+
+
+// task3
+// let i = 100;
+// while (i <=999){
+//     if (i % 5 === 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// task4
+// let eded = prompt("eded daxil edin :");
+// let sum = 0;
+// let i = 100;
+// while (i <=999){
+//     if (i % eded === 0){
+//         sum += i;
+//     }
+//          i++;
+// }
+
+// task7 
+// let hasil = 1;
+// for (let i=1; i <=10; i++){
+//     hasil *=1;
+// }
+
+// task 8
+// let metn = prompt("metn daxil edin!");
+// let reversedMetn = "";
+// for let i = metn.length - 1; i >= 0; i--){
+//     reversedMetn += text [i];
+// }
+// console.log(reversedMetn)
