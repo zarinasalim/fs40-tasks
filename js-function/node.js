f28c37e71b37dc0dd986d2a6d623c30d40512c18
zarinasalim <salimovazarina72@gmail.com>
2025-06-03 17:25:56 +0400

    function-tasks

---
+++ js-function/node.js
@@ -0,0 +1,77 @@
+// task1
+// function istifadeciAdi (username){
+//     console.log("${username} is active now");
+// }
+// istifadeciAdi("Mariam");
+// istifadeciAdi("Lara");
+// istifadeciAdi("Medina");
+
+
+// task2
+// function ededler (a,b,c){
+//     let result;
+//     if ( a >= b && a >= c ) {
+//         result = a;
+//     } else if ( b >= a && b >= c ){
+//         result = b;
+//     } else ( c >= a && c >= b) {
+//         result = c;
+//     }
+// }
+
+
+// // task5
+// function minEded (a,b){
+//     if (a>b){
+//         return b;
+//     }
+//     else (b>a){
+//         return a;
+//     }
+// }
+
+
+
+// task6
+// function add (a,b,c){
+//     switch (c) {
+//         case "+":
+//         return a+b;
+//         case "-":
+//             return a-b;
+//             case "*":
+//                 return a*b;
+//                 case "/":
+//                     return a/b;
+//                     default:
+//                         return "yanlishdir!"
+//     }
+// }
+// console.log (add (5,7, "+"));
+// console.log (add (10,3, "-"));
+// console.log (add (5,9, "*"));
+// // console.log (add (42,6, "/"));
+
+
+
+// // task9
+// function isEven (number){
+//     if (numer % 2 === 0){
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+
+
+// task11
+// function isPrime (number){
+//     if ( number <= 1) {
+//         return false;
+//     } else if (number  === 2 ){
+//         return true;
+//     }
+//      else if ( number % 2 === 0 ){
+//         return false; 
+//      }
+// }
