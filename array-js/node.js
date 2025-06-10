2364e8cd36b7c279a0763cebb36a8804ded27f3d
zarinasalim <salimovazarina72@gmail.com>
2025-06-10 19:04:25 +0400

    srcipt.js

---
+++ array-js/node.js
@@ -0,0 +1,56 @@
+// 1.Üç addan ibarət massiv yaradın.
+// Massivin ikinci elementinin dəyərini “Classified” ilə əvəz edin.
+// Massivi konsola çıxarın.
+
+// const user = [" Fira", "Sultan", "Narmin"];
+// user[1] = "Classified";
+// console.log(user);
+
+
+
+
+// 2.Massiv verilib. "Episode 4: New Hope" kimi mesajları ardıcıl olaraq konsola çıxarın.
+// Massiv elementlərin düzün.
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+// //Episode 4: New Hope
+// //Episode 5: Empire strikes back
+// //Episode 6: Return of the Jdi
+
+
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+// console.log (arr [0]);
+// console.log(arr [1]);
+// console.log( arr [2]);
+
+
+
+// // 5 ```Verilən mətn tip dəyərdəki vergülləri nöqtəli vergüllə əvəz edin.
+// '32, 31, 34, 36, 31' mətni verilmişdir ,İçindəki vergülləri nöqtəli
+// vergüllə əvəz edin.(Massivden sitifade ederek)(join)
+// // let num = '32, 31, 34, 36, 31';
+// let array = num.split(',');
+// let yeniNum = array.join(';');
+// console.log("Orijinal mətn:", num);
+// console.log("Dəyişdirilmiş mətn:", yeniNum , num );
+
+
+
+// task6
+// let group = ["Əli", "Ayşə", "Murad"];
+
+// group.push("Leyla");
+
+// group.shift();
+
+// console.log(group);
+
+
+
+// task8
+// const numbers = [12, 45, 6, 89, 23, 7, 56, 91, 1, 34];
+// const maxNumber = Math.max(...numbers);
+// console.log("Massivdəki maksimal dəyər:", maxNumber);
+
+
+
+
