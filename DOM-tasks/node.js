d08c522758bb0b78f3ab487a0998167ae8291bcd
zarinasalim <salimovazarina72@gmail.com>
2025-06-21 01:29:19 +0400

    dom-tasks

---
+++ DOM-tasks/node.js
@@ -0,0 +1,38 @@
+// const title = document.querySelector ("h1")
+// console.log(title);
+
+
+// const title = document.querySelector("h1").remove();
+
+// const list = document.createElement ("li");
+// list.textContent = "TITAN RTX";
+// const id = document.querySelector("ul")
+// id.appendChild(list);
+
+
+// const title = document.querySelector ("h1")
+// title.textContent = "GeForse 20 Series";
+
+// const Elements = document.querySelectorAll('.primary');
+//  Elements.forEach(Element =>{
+//      Element.style.backgroundColor = "#ebebeb"
+// })
+
+
+// const primary = document.querySelectorAll (".primary");
+// primary.forEach (primary => {
+//     console.log(primary)
+// })
+
+// const list = document.querySelectorAll ("li");
+// list.forEach (list => {
+//     list.classList.add ("item");
+// })
+
+
+// const list = document.querySelectorAll ("li");
+//  list.forEach(item => {
+//     if (!item.classList.contains ("primary")) {
+//         item.classList.add ("secondry");
+//     }
+//  });
